d6b5e60f6d84c856f98c1994e6c40130624410aa
YASAN <yasandev@gmail.com>
2022-05-09 22:43:19 +0430

    Create KaizenApi.kt

---
+++ app/src/main/java/dev/yasan/kaizen/data/network/KaizenApi.kt
@@ -0,0 +1,14 @@
+package dev.yasan.kaizen.data.network
+
+import dev.yasan.kaizen.model.Sport
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+
+interface KaizenApi {
+
+    @Headers("Accept: application/json")
+    @GET("/sports")
+    suspend fun getSports(): Response<List<Sport>>
+
+}
